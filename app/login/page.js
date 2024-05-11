22d6a07a17920dab4948e440dfcdef5a7c19f7f4
Dawood Faisal <dawood_faisal@yahoo.com>
2024-05-11 17:34:43 +0500

    commit-6 login page fix

--- app/login/page.js
+++ app/login/page.js
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className='text-white py-14 container mx-auto'>
       <h1 className='font-bold text-center text-3xl'>
-        Login/Signup to get your fans to support you
+      Login to Get Started
       </h1>
     </div>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <li>Contact</li>
       </ul> */}
 
-      <div>
+      <div className='flex items-center justify-center'>
         <Link href={"/login"}>
         <button type='button' className='"text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>
           Login
