6ab40229c59a438e2858c118abe2597d915382ca
Dawood Faisal <dawood_faisal@yahoo.com>
2024-05-15 14:47:09 +0500

    commit-13 username page.js ui updated

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -23,26 +23,47 @@ const Username = ({ params }) => {
 
         <div className="payment flex gap-3 w-[80%] mt-10">
           <div className="supporters w-1/2 bg-slate-900 text-white p-4 rounded-lg">
-            {/* show list of all the supporters as a leaderboard */}
             <h2 className='text-2xl font-bold my-3'>Supporters</h2>
-            <ul className='mx-4'>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
-              <li className='my-2'>Xyz donated $30 with a message ""</li>
+            <ul className='mx-4 text-lg'>
+
+              <li className='my-2 flex gap-2 items-center'>
+                <img width={33} src="avatar.gif" alt="avatar" />
+                <span>
+                  Xyz donated <span className="font-bold">$30</span> with a message "I support you! Lots of ❤️"
+                </span></li>
+              <li className='my-2 flex gap-2 items-center'>
+                <img width={33} src="avatar.gif" alt="avatar" />
+                <span>
+                  Xyz donated <span className="font-bold">$30</span> with a message "I support you! Lots of ❤️"
+                </span></li>
+              <li className='my-2 flex gap-2 items-center'>
+                <img width={33} src="avatar.gif" alt="avatar" />
+                <span>
+                  Xyz donated <span className="font-bold">$30</span> with a message "I support you! Lots of ❤️"
+                </span></li>
+
             </ul>
           </div>
 
           <div className="makePayment w-1/2 bg-slate-900 text-white p-2 rounded-lg">
             <h2 className='text-2xl font-bold my-3'>Make a Payment</h2>
-            <div className='flex gap-3'>
-              <input className='w-full p-4 rounded-lg bg-slate-800' placeholder="Enter amount" />
-              <button className='bg-slate-800 p-5 rounded-lg'>Pay</button>
+
+            <div className='flex gap-2 flex-col'>
+              <input type='text' className='w-full p-3 rounded-lg bg-slate-800' placeholder="Enter Name" />
+              <input type='text' className='w-full p-3 rounded-lg bg-slate-800' placeholder="Enter Message" />
+              <input type='text' className='w-full p-3 rounded-lg bg-slate-800' placeholder="Enter Amount" />
+              <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
             </div>
+            {/* Or choose from these amounts */}
+
+            <div className='flex gap-2 mt-3 items-center justify-center'>
+              <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay $10</button>
+              <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay $20</button>
+              <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay $50</button>
+            </div>
+
           </div>
+
         </div>
       </div>
     </>
