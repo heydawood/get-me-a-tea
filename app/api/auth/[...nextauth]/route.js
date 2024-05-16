ac40355bcdcac7d4e29ba5a50d1ccf6f6288d3af
Dawood Faisal <dawood_faisal@yahoo.com>
2024-05-16 16:31:27 +0500

    commit-18 created User & Payment models

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,12 @@
 // import FacebookProvider from 'next-auth/providers/facebook'
 // import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
+
 import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
+import mongoose from 'mongoose';
+import User from '@/app/models/User';
+import Payment from '@/app/models/Payment';
 
 export const authoptions = NextAuth({
     providers: [
@@ -12,35 +16,32 @@ export const authoptions = NextAuth({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-        //       AppleProvider({
-        //         clientId: process.env.APPLE_ID,
-        //         clientSecret: process.env.APPLE_SECRET
-        //       }),
-        //       FacebookProvider({
-        //         clientId: process.env.FACEBOOK_ID,
-        //         clientSecret: process.env.FACEBOOK_SECRET
-        //       }),
-        //       GoogleProvider({
-        //         clientId: process.env.GOOGLE_ID,
-        //         clientSecret: process.env.GOOGLE_SECRET
-        //       }),
-        //       // Passwordless / email sign in
-        //       EmailProvider({
-        //         server: process.env.MAIL_SERVER,
-        //         from: 'NextAuth.js <no-reply@example.com>'
-        //       }),
     ],
-
+    
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
-            if(account.provider == 'github') {
+            if(account.provider == "github") {
                 // connect to the database
-                const client = await mongoose.connect()
+                const client = await mongoose.connect("mongodb://localhost:27017/getmeatea")
                 //check if the user exists in the database 
-                //const user = await client.db('users').collection('users').findOne({email: email})
+                const currentUser = User.findOne({email: email})
+                if(!currentUser)
+                {
+                    //create a new user
+                    const newUser = new User({
+                        email: email,
+                        username: email.split("@")[0],
+                    })
+                    await newUser.save()
+                    user.name = newUser.username
+                }
+                else{
+                    user.name = currentUser.username
+                }
+                return true
             }
         }
-    }
+    }    
 })
 
 export { authoptions as GET, authoptions as POST }
